62a641880191016666b8f5ec7b0de0a2f2ac08d6
chichipan <a1567149d@fie.umich.mx>
2021-06-26 12:55:49 -0500

    funcional falta checar los de los chats

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_xmpp/pages/usuario_model.dart';
 import 'package:xmpp_stone/xmpp_stone.dart' as xmpp;
 
 class ChatPage extends StatefulWidget {
@@ -20,34 +21,41 @@ class _ChatPageState extends State<ChatPage> {
 
   _ChatPageState(this.jidDest,this._connection);
 
+  xmpp.Jid xjidDes;
   String jidDest;
   xmpp.Connection _connection;
+  
   xmpp.MessageHandler _messageHandler;
-  xmpp.Jid xjidDes;
 
+  List<MessageChat> _messages = [];
+
+  TextEditingController _messageController = TextEditingController();
+
+  var subs;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-    xjidDes = xmpp.Jid.fromFullJid('flutter_test1@jabjab.de');
+    xjidDes = xmpp.Jid.fromFullJid(jidDest);
 
     _messageHandler = xmpp.MessageHandler.getInstance(_connection);
 
-    _messageHandler.messagesStream.listen((xmpp.MessageStanza message) {
-      print('este es el evento');
-      print(message.body);
+     subs = _messageHandler.messagesStream.listen((xmpp.MessageStanza message) {
+      print(message.thread);
+      setState(() {
+      _messages.insert(0,MessageChat(jid: message.fromJid.fullJid,text: message.body));
+      });
     });
-
-
   }
 
   @override
   void dispose() {
-  super.dispose();
-    
+    super.dispose();
 
+    _messageController?.dispose();
+    subs.cancel();
   }
 
   @override
@@ -56,9 +64,108 @@ class _ChatPageState extends State<ChatPage> {
       appBar: AppBar(
         title: Text(this.jidDest),
       ),
-      body: Center(
-        child: Text('chat'),
+      body: Container(
+        color: Colors.black12,
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                
+                itemCount: _messages.length,
+                itemBuilder: (ctx,i) => _messages[i],
+                reverse: true,
+              )
+            ),
+            Container(
+              color: Colors.transparent,
+              padding: EdgeInsets.symmetric(horizontal: 5),
+              child: Row(
+                children: [
+                  _inputMessage(),
+                  IconButton(
+                    color: Colors.blue,
+                    icon: Icon(Icons.send),
+                    onPressed: _handleSend
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )
+    );
+  }
+
+  _handleSend(){
+
+    if(_messageController.text.length > 0){
+
+      _messageHandler.sendMessage(xjidDes,_messageController.text);
+      _messages.insert(0,MessageChat(jid:Usuario.getUsuario.jid,text:_messageController.text));
+      setState(() {
+        _messageController.text = '';
+      });
+    }
+
+  }
+
+  Widget _inputMessage(){
+    return Flexible(
+      child: SafeArea(
+          child: Container(
+          height: 40,
+          margin: EdgeInsets.symmetric(vertical: 8),
+          padding: EdgeInsets.symmetric(horizontal: 5),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20)
+          ),
+          child: TextField(
+            controller: _messageController,
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: 'Escribe'
+            ),
+          ),
+        ),
       ),
     );
+  }
+}
+
+class MessageChat extends StatelessWidget {
+  
+  final String text;
+  final String jid;
+
+  const MessageChat({Key key, this.text, this.jid}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return (jid == Usuario.getUsuario.jid)
+      ? Align(
+        alignment: Alignment.bottomRight,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 5,horizontal: 10),
+          margin: EdgeInsets.only(bottom: 5,left: 100,right: 5),
+          decoration: BoxDecoration(
+            color: Colors.green[300],
+            borderRadius: BorderRadius.circular(100)
+          ),
+          child: Text(text,style: TextStyle(color: Colors.white),),
+        ),
+      )
+      :Align(
+        alignment: Alignment.bottomLeft,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 5,horizontal: 10),
+          margin: EdgeInsets.only(bottom: 5,left: 5,right: 100),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(100)
+          ),
+          child: Text(text),
+        ),
+      );
   }
 }
